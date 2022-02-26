45b6e1ead56641f0769b1541be8c96ea6d722449
emre <piskin.emre@gmail.com>
2022-02-26 22:56:10 +0100

    Add encoding support for url requests Update cheerio version

--- sources/en-rss-ua-pravda.js
+++ sources/en-rss-ua-pravda.js
@@ -4,5 +4,6 @@ module.exports.ENDPOINT = '/en-rss-ua-pravda';
 module.exports.get = (cache) => getRss({
   cache,
   cacheKey: 'en-rss-ua-pravda',
+  encoding: 'win1251',
   sourceUrl: 'https://www.pravda.com.ua/eng/rss',
 });
